512203c9b00230312ba5581096987c791baee64a
ilyaas <ilyaascumar10@gmail.com>
2023-02-28 08:48:03 +0300

    public directory is changed

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Layout from "../components/Layout";
 import Home from "../pages/Home";
